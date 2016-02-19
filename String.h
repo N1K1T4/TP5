2643d63f80358dd2e9b4a955e91b33c832f60ad6
Nikita Mauvisseau <nikita.mauvisseau@insa-lyon.fr>
2016-02-19 10:24:21 +0100

    String.h fini

--- String.h
+++ String.h
@@ -1,54 +1,70 @@
-
-
 #ifndef STRING_H__
 #define STRING_H__
 
-#include <stdio.h>
-#include <cstdlib>
-
-
-class String{
+class String
+{
 public:
 //======================================================================
-//             CONSTRUCTEURS & DESTRUCTEUR
+//             CONSTRUCTORS & DESTRUCTOR
 //======================================================================
   String();
   String(size_t capacity);
   String(size_t capacity, char c);
-  String(const String& str); //par copie
-  String(const char* s); //cstring
-  ~String(); 
-
+  String(const String& s); // copy constructor
+  String(const char* s); // c-string constructor
+  ~String();
+//======================================================================
+//             GETTERS
+//======================================================================
+  inline size_t capacity() const;
+  inline size_t length() const;
+  inline size_t max_size() const;
+  inline size_t size() const;
 //======================================================================
 //             PUBLIC METHODS
 //======================================================================
-  void resize(size_t);
-  inline size_t size() const;
-  inline size_t capacity() const;  
   bool empty();
+	char* c_str();
+	void clear();
+  void resize(size_t);
   void reserve(size_t length);
-  void operator=(char*);
-
-  static size_t size_max;
+//======================================================================
+//             PUBLIC OPERATORS
+//======================================================================
+	const String& operator = (const char c);
+  const String& operator = (const char* s);
+	const String& operator = (const String& s);
+	void operator + (const char c);
+	void operator + (const char* s);
+	void operator + (const String s);
+private:
+//======================================================================
+//             PRIVATE ATTRIBUTES
+//======================================================================
+  static size_t _max_size;
   size_t _size;
   size_t _capacity;
   char* _str;
 };
 
-inline size_t String::size() const{
-	return _size;
-}
-
-inline size_t String::capacity() const{
+inline size_t String::capacity() const
+{
 	return _capacity;
 }
 
-inline size_t String::length() const{
+inline size_t String::length() const
+{
   return _size;
 }
 
-inline size_t String::max_size() const{
-  return size_max;
+inline size_t String::max_size() const
+{
+  return _max_size;
+}
+
+inline size_t String::size() const
+{
+	return _size;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <cstdio>
+#include <cstdlib>
 #include "String.h"
 
-
-int main(){
+int main()
+{
   String s0 = String();
   String s1 = String(5);
   printf("%lu \n",s0.size());
@@ -13,5 +15,4 @@ int main(){
   printf("%lu \n",s1.size());
   printf("%lu \n",s1.length());
   printf("%lu \n",s1.max_size());
-
 }
